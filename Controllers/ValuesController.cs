84bda953f92390efd546ba408bd9a901c90b5b77
Thiruvarasan S <thirus@thoughtworks.com>
2019-04-19 15:59:37 -0400

    update namespace

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace reference_app.Controllers
+namespace ReferenceApp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace reference_app
+namespace ReferenceApp
 {
     public class Program
     {
