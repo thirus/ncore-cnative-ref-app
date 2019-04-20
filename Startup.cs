bcc85e8702da09fce21ab652e68085a0c1660eeb
Thiruvarasan S <thirus@thoughtworks.com>
2019-04-19 22:41:36 -0400

    add jaeger tracer

--- Startup.cs
+++ Startup.cs
@@ -26,6 +26,8 @@ namespace ReferenceApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddJaeger();
+            services.AddOpenTracing();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
